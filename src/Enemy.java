069a67fe00941f34981b53fc826bddb63a22c3f6
javierestrada <estrada.javier.525@gmail.com>
2021-09-20 09:10:15 -0500

    feat: Saving DATA as properties of Enemy, and evoking the talk method

---
+++ src/Enemy.java
@@ -0,0 +1,11 @@
+public class Enemy {
+
+//  ----------  Adding attributes (DATA) to the Enemy class.
+    int health;
+
+    public void Talk() {
+        System.out.println("Warning!!! I am an enemy. You better run.");
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        
+//  ----------  Creating an Enemy object
+    Enemy enemy = new Enemy();
+
+//  ----------  Envoking the talk method (Function).
+    enemy.Talk();
 
 
     }
-
-
 }
